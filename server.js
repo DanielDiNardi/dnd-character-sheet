9ca6f3931fbabd32a65a3fbbb9730a06da0ff38d
Daniel Di Nardi <dan.dinardi@hotmail.com>
2023-07-09 13:35:49 +0100

    added bootstrap, began with UI layout, passed HTML and CSS files to front-end

--- server.js
+++ server.js
@@ -4,12 +4,15 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.use(express.static(__dirname + '/public'));
+
 var characterRawData = fs.readFileSync("./characters/Roderick the Reaver.json");
 var character = JSON.parse(characterRawData);
 
 app.get('/', (req, res) => {
-    res.header("Content-Type",'application/json');
-    res.send(JSON.stringify(character, null, 4));
+    // res.header("Content-Type",'application/json');
+    // res.send(JSON.stringify(character, null, 4));
+    res.sendFile(__dirname + "/public/charactersheet.html");
 })
 
 app.listen(port, () => {
