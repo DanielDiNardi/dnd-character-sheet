5751c0b18b8b43416ed3c5bb22ff791382d8651d
Daniel Di Nardi <dan.dinardi@hotmail.com>
2023-08-05 15:07:53 +0100

    Added ability and saving throw buttons, and created GetValue function

--- public/javascript/generateAttacks.js
+++ public/javascript/generateAttacks.js
@@ -5,25 +5,38 @@ function Attacks(characterData){
     console.log(characterData);
 
     var attackModifierSplitArray = characterData["weapon-attack-bonus-1"].split("+")
+    console.log(attackModifierSplitArray);
+    // console.log(GetValues(characterData["weapon-attack-bonus-1"], "+"));
     var attackModifierString = attackModifierSplitArray[1];
     var attackModifier = parseInt(attackModifierString);
-    console.log(attackModifier);
+    // console.log(attackModifier);
 
+    // IMPORTANT
     var damageQuantitySplitArray = characterData["weapon-damage-1"].split("d");
+    // console.log(GetValues(characterData["weapon-damage-1"], "+"));
     var damageQuantityString = damageQuantitySplitArray[0];
     var damageQuantity = parseInt(damageQuantityString);
-    console.log(damageQuantity);
+    // console.log(damageQuantity);
 
+    // IMPORTANT
     var damageSplitArray = damageQuantitySplitArray[1].split("+");
+    // console.log(GetValues(damageQuantitySplitArray[1], "+"));
     var damageString = damageSplitArray[0];
     var damage = parseInt(damageString);
-    console.log(damage);
+    // console.log(damage);
 
 
     var damageModifierSplitArray = damageSplitArray[1].split(" ");
+    console.log(GetValues(damageSplitArray[1], " "));
     var damageModifierString = damageModifierSplitArray[0];
     var damageModifier = parseInt(damageModifierString);
     console.log(damageModifier);
 }
 
 // make function that does the above ^^
+function GetValues(array, splitValue) {
+    var splitArray = array.split(splitValue);
+    splitArray = splitArray.filter((value) => value != "");
+    var splitArrayString = splitArray[0];
+    return parseInt(splitArrayString);
+}
